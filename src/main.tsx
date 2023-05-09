76d5d44f37cf700e06d9ebc00fb689e69cde08bd
Kangonga <guchu413@gmail.com>
2023-05-09 11:54:46 +0300

    feat: change rrouter to createBrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from '@/App'
+
+import {
+  createBrowserRouter,
+  RouteObject,
+  RouterProvider
+} from 'react-router-dom';
+
+
+const routes:RouteObject[] = [
+  {
+    path:'/',
+    element: <App />,
+    errorElement:<h1>Error: Page not found</h1>,
+  },
+]
+
+const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
