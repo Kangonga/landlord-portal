033fd7145d3bfba5fd728442404a6ee1929fe9b1
Kangonga <guchu413@gmail.com>
2023-05-10 16:44:43 +0300

    style: create container styles

---
+++ src/shared/components/sharedStyles.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@mui/material";
+
+export const Container = styled('div')(({ theme })=>({
+    display:'flex',
+}))
+export const ComponentContainer = styled('div')(({ theme })=>({
+    display:'flex',
+    flexDirection:'column',
+    flex:'2'
+}))
