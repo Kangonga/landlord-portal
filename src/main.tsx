b37b3780d8fef80a713df9e59007db40f19d4839
Kangonga <guchu413@gmail.com>
2023-05-10 19:45:26 +0300

    refactor: change the way createBrowserRouter is declared

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
-
 import {
   createBrowserRouter,
-  RouteObject,
+  createRoutesFromElements,
+  Route,
   RouterProvider
 } from 'react-router-dom';
 
+import Home from './features/home/Home';
+import BuildingHomePage from './features/buildingFeatures/BuildingHomePage';
+import PageNotFound from './shared/components/pageNotFound/PageNotFound';
 
-const routes:RouteObject[] = [
-  {
-    path:'/',
-    element: <App />,
-    errorElement:<h1>Error: Page not found</h1>,
-  },
-]
 
-const router = createBrowserRouter(routes)
+// const routes:RouteObject[] = [
+//   {
+//     path:'/',
+//     element: <App />,
+//     errorElement:<h1>Error: Page not found</h1>,
+//   },
+// ]
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+        <Route index element={<Home />} />
+        <Route path='/buildings' element={<BuildingHomePage />} />
+        <Route path='*' element={<PageNotFound />} />
+    </Route>
+  )
+)
+
+// const router = createBrowserRouter(routes)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
