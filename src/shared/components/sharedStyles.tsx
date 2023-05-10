69be878dd79c7fff56d82ad13f916d5953a8e9cd
Kangonga <guchu413@gmail.com>
2023-05-10 19:48:24 +0300

    style: add background color to the component comtainer

--- src/shared/components/sharedStyles.tsx
+++ src/shared/components/sharedStyles.tsx
@@ -1,10 +1,11 @@
 import { styled } from "@mui/material";
 
-export const Container = styled('div')(({ theme })=>({
+export const Container = styled('div')(({  })=>({
     display:'flex',
 }))
-export const ComponentContainer = styled('div')(({ theme })=>({
+export const ComponentContainer = styled('div')(({  })=>({
     display:'flex',
     flexDirection:'column',
-    flex:'2'
+    flex:'2',
+    backgroundColor:'#e0e7ff'
 }))
