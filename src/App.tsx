188320e915ec6b4a681276d6b80c4075d23b3d30
Kangonga <guchu413@gmail.com>
2023-05-10 19:46:45 +0300

    feat: render pages using outlet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Sidebar from "@/shared/sidebar/Sidebar" 
 import { ComponentContainer, Container } from "./shared/components/sharedStyles"
 import Topbar from "./shared/topbar/Topbar"
+import { Outlet } from "react-router-dom"
 export default function App() {
   return (
     <Container>
         <Sidebar />
         <ComponentContainer>
           <Topbar />
+            <Outlet />
         </ComponentContainer>
     </Container>
   )
