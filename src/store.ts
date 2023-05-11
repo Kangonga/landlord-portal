d3487138f2987d1f0cdd4b3eebc455ac3fe4d856
Kangonga <guchu413@gmail.com>
2023-05-11 21:21:26 +0300

    fear: add reducer placeholders

--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '@/features/authentication/authSlice'
 import buildingReducer from './features/buildingFeatures/buildingSlice';
 import meterReducer from './features/mainMeterFeatures/meterSlice';
+import utilityReducer from "./features/utilitySlice";
 
+export interface storeInterface{
+    auth:()=>{},
+    building:()=>{},
+    meter:()=>{},
+    utility:()=>{}
+}
 export const store = configureStore({
     reducer: {
         auth:authReducer,
         building: buildingReducer,
         meter: meterReducer,
+        utility: utilityReducer
     }
 })
