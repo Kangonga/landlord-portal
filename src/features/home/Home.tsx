641374ffae19b4ab099357c448601f498ff16432
Kangonga <guchu413@gmail.com>
2023-05-11 14:31:38 +0300

    feat: display the recently created components on the homepage

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import BuildingWidgets from "@/features/buildingFeatures/components/widgets/BuildingWidgets"
+import { Bar, BarChartContainer } from "../buildingFeatures/pages/styles"
+import { HomeContainer } from "./styles"
+import BuildingStatements from "../buildingFeatures/components/buildingStatements/BuildingStatements"
 
 export default function Home() {
     return (
-      <div>
-
-      </div>
+      <HomeContainer>
+          <BuildingWidgets />
+          <BarChartContainer>
+            <Bar />
+            <BuildingStatements />
+          </BarChartContainer>
+      </HomeContainer>
   )
 }
