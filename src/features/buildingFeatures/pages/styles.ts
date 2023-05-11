81c5c05148c68171ad3f36046cd3f4c6f1a8bb10
Kangonga <guchu413@gmail.com>
2023-05-11 14:31:14 +0300

    feat: create div with dimensions to mock bar chart

--- src/features/buildingFeatures/pages/styles.ts
+++ src/features/buildingFeatures/pages/styles.ts
@@ -2,8 +2,21 @@ import { styled } from "@mui/material";
 
 export const BarChartContainer = styled('div')(({ theme })=>({
     display:'flex',
-
+    gap:'.4rem',
+    flexWrap:'wrap',
     [(theme.breakpoints.down('sm'))]:{
         flexDirection:'column',
+    },
+    '#barchart':{
+        minWidth:"600px"
     }
+}))
+
+export const Bar = styled('div')(({ theme })=>({
+    height:'500px',
+    width:'700px',
+    backgroundColor:'gray',
+    [( theme.breakpoints.down('lg' ))]:{
+        width:'100%'
+    },
 }))
