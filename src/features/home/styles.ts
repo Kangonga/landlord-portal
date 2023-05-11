534a43f7b31994bb6c4094f6aedf2e0d535fd987
Kangonga <guchu413@gmail.com>
2023-05-11 14:30:42 +0300

    style: create styles for the homepage container

---
+++ src/features/home/styles.ts
@@ -0,0 +1,8 @@
+import { styled } from "@mui/material";
+
+export const HomeContainer = styled('div')(({})=>({
+    display:'flex',
+    flexDirection:'column',
+    gap:'1rem',
+    padding:'.75rem'
+}))
