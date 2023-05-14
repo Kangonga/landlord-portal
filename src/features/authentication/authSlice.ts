bf5fe9e52bcdda74f15f485afb9f2e8ea2d29788
Kangonga <guchu413@gmail.com>
2023-05-14 21:16:48 +0300

    feat: add basic login functionality to authslice

--- src/features/authentication/authSlice.ts
+++ src/features/authentication/authSlice.ts
@@ -1,22 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {
+interface authInterface {
+    loading: boolean,
+    userInfo: {},
+    utility: string,
+    userToken: string,
+    error: string|undefined,
+    isLoggedIn: boolean,
+    hasBothUtilities: boolean
+}
+const initialState:authInterface = {
     loading: false,
     userInfo: {},
     utility: '',
-    userToken: null,
-    error: null,
+    userToken: '',
+    error: '',
     isLoggedIn: false,
     hasBothUtilities: false
 }
+
+const login = createAsyncThunk('auth/login',async ()=>{
+    const response = await axios.post('api/login');
+    return response.data;
+})
 
 const authReducer = createSlice({
     name:'auth',
     initialState,
     reducers:{
         logout: ()=> initialState
+    },
+    extraReducers: builder=>{
+        builder
+        .addCase(login.pending, state=>{
+            state.loading = true
+        })
+        .addCase(login.fulfilled, (state, action)=>{
+            state.loading = false
+            state.userInfo = action.payload
+            state.isLoggedIn = true
+            state.userToken = action.payload.userToken
+            state.hasBothUtilities = action.payload.utilityType>1?true:false
+        })
+        .addCase(login.rejected, (state, action)=>{
+            state.loading = false
+            state.error = action.error.message
+        } )
     }
 })
 
 export default authReducer.reducer
 export const authActions = authReducer.actions
+export { login }
