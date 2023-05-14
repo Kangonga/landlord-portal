ef643ef74e7ba2d529698ad66e6cc784198e3df7
Kangonga <guchu413@gmail.com>
2023-05-14 15:51:30 +0300

    feat: display building data table on homepage

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -2,6 +2,7 @@ import BuildingWidgets from "@/features/buildingFeatures/components/widgets/Buil
 import { Bar, BarChartContainer } from "../buildingFeatures/pages/styles"
 import { HomeContainer } from "./styles"
 import BuildingStatements from "../buildingFeatures/components/buildingStatements/BuildingStatements"
+import BuildingDataTable from "../buildingFeatures/components/buildingDataTable/BuildingDataTable"
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <Bar />
             <BuildingStatements />
           </BarChartContainer>
+          <BuildingDataTable />
       </HomeContainer>
   )
 }
