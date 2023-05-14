8533410ce0b2e6e21d501a9a8f90c45fdc75ede0
Kangonga <guchu413@gmail.com>
2023-05-14 15:51:08 +0300

    feat: create the building data Building Data Table

---
+++ src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { columns, rows } from './dataStructure'
+
+export default function BuildingDataTable() {
+  return (
+    <Box>
+        <DataGrid 
+            columns={columns}
+            rows={rows}
+            slots={{
+                toolbar:GridToolbar
+              }}
+            sx={{
+                height:'max-content',
+                overflow:'auto',
+                paddingBottom:'1rem'
+            }}
+        />
+    </Box>
+  )
+}
