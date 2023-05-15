a86ae1d7484dd055045b3c0ace2098336c16c860
Kangonga <guchu413@gmail.com>
2023-05-15 11:38:38 +0300

    feat: make the login form controlled

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,33 @@
 import { images } from '@/assets'
 import { LoginForm, LoginPageContainer } from './styles'
 import { Button, Typography } from '@mui/material'
-import { CheckBox } from '@mui/icons-material'
+import { useAppSelector } from '@/globalHooks';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function Login() {
-  const handleSubmit = ()=>{
+  const [formData, setformData] = useState({
+    email: '',
+    password:'',
+    keepLoggedIn: false
+  })
+  const authState = useAppSelector(state=>state.auth)
+  const handleSubmit = (e:FormEvent)=>{
+    e.preventDefault()
+    console.log(formData)
+  }
+  const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
+    if(e.target.name==='keepLoggedIn'){
+      console.log(formData.keepLoggedIn)
+       setformData({
+        ...formData,
+        keepLoggedIn:(!formData.keepLoggedIn)
+      })}  
+    else setformData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+    console.log(e.target.name)
+    console.log(e.target.value)
   }
   return (
     <LoginPageContainer>
@@ -15,19 +38,26 @@ export default function Login() {
         <Typography className='formTitle'>
           Landord Login
         </Typography>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit} >
           <div className='formInput'>
             <label htmlFor="email">Email</label>
-            <input id='name' type='email'/>
+            <input value={formData.email}id='email' name='email' type='email'onChange={handleChange}/>
           </div>
           <div className='formInput'>
             <label htmlFor="password">Password</label>
-            <input id='password' type='password'/>
+            <input value={formData.password} id='password' name='password' type='password'onChange={handleChange}/>
           </div>
-          <Button className='button'>Sign In</Button>
-          <div className='keepLoggedInSection'>Keep me signed in
-              <input type='checkbox' style={{marginLeft:'1rem'}} />
+          {authState.error&&<Typography textAlign='center' margin='.75rem' color='red'>Invalid email or password</Typography>}
+          <div className='keepLoggedInSection'>
+            <label>Keep me signed in</label>
+              <input 
+              checked={formData.keepLoggedIn}
+              value={String(formData.keepLoggedIn)}
+              type='checkbox'
+              onChange={handleChange}
+              name='keepLoggedIn' style={{marginLeft:'1rem'}}/>
           </div>
+          <Button className='button' type='submit'>Sign In</Button>
         </form>
       </LoginForm>
     </LoginPageContainer>
