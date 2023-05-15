2af14e276f1ba0243a1704a8fe62948d47c43ec5
Kangonga <guchu413@gmail.com>
2023-05-15 21:51:14 +0300

    style: add flex wrap

--- src/features/mainMeterFeatures/components/mainMeterDataTable/styles.tsx
+++ src/features/mainMeterFeatures/components/mainMeterDataTable/styles.tsx
@@ -1,11 +1,9 @@
 import { styled } from "@mui/material";
 
-export const MeterContainer = styled('div')(({ theme })=>({
+export const MeterContainer = styled('div')(({ })=>({
     display:'flex',
     flexDirection:'row',
+    flexWrap:'wrap',
     gap:'1rem',
     padding:'.75rem',
-    [(theme.breakpoints.down('lg'))]:{
-        flexDirection:'column'
-    }
 }))
