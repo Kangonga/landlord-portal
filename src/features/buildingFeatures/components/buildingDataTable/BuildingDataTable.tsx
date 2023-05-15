4b1e5047e3757bfa9111d067380679a985267d48
Kangonga <guchu413@gmail.com>
2023-05-15 16:30:57 +0300

    feat: create a title for the  data table

--- src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
+++ src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
@@ -1,10 +1,11 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { DataGrid, GridToolbar } from '@mui/x-data-grid'
 import { columns, rows } from './buildingTableDataStructure'
 
 export default function BuildingDataTable() {
   return (
     <Box>
+      <Typography sx={{fontSize:'1.2rem', textAlign:'center', color:'gray'}}>Building summary</Typography>
         <DataGrid 
             columns={columns}
             rows={rows}
