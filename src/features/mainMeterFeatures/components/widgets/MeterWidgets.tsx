68c46e97f6636def6f69ccb04cc350276c3ade58
Kangonga <guchu413@gmail.com>
2023-05-15 21:04:33 +0300

    feat: create meter widgets

---
+++ src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
@@ -0,0 +1,42 @@
+import { useAppSelector } from '@/globalHooks'
+import { MeterWidget, WidgetsContainer } from './styles'
+import { Autocomplete, TextField } from '@mui/material'
+
+export default function MeterWidgets() {
+const activeBuildingDetails = useAppSelector(state=>state.utility.activeBuilding)
+console.log('active building', activeBuildingDetails)
+  return (
+        <WidgetsContainer>
+            <MeterWidget>
+                    <span id='clientLabel'>Client:</span>
+                    <span id='clientName'>John Doe</span>
+            </MeterWidget>
+            <MeterWidget>
+                    <span id='clientLabel'>Building Name:</span>
+                    <span id='clientName'>{activeBuildingDetails.label}</span>
+            </MeterWidget>
+            <Autocomplete
+                disablePortal
+                sx={{ width:'200px' }}
+                id="combo-box"
+                options={ ['123134421','873243432','all meters'] }
+                renderInput={(params) => 
+                <TextField {...params} label='Select meter'
+                />
+                }
+                noOptionsText='No meter found'
+           />
+           <Autocomplete
+                disablePortal
+                sx={{ width:'200px' }}
+                id="combo-box"
+                options={ ['123134421','873243432','all m-paya meters'] }
+                renderInput={(params) => 
+                <TextField {...params} label='Select m-paya meter'
+                />
+                }
+                noOptionsText='No meter found'
+           />
+        </WidgetsContainer>
+   )
+}
---
+++ src/features/mainMeterFeatures/components/widgets/styles.ts
@@ -0,0 +1,23 @@
+import { styled } from "@mui/material";
+
+export const WidgetsContainer = styled('div')(({})=>({
+    display:'flex',
+    gap:'1rem',
+    flexWrap:'wrap'
+}))
+
+export const MeterWidget = styled('div')(({})=>({
+    display:'flex',
+    gap:'.3rem',
+    backgroundColor:'#a5b4fc',
+    height: 'max-content',
+    padding:'.75rem',
+    borderRadius:'5px',
+    '#clientLabel':{
+        color:'#374151',
+        fontWeight:'500'
+    },
+    '#clientName':{
+        fontWeight:'700'
+    }
+}))
