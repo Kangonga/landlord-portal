b80a00a90e44162e2fb93ec41f556df23d83035b
Kangonga <guchu413@gmail.com>
2023-05-17 22:32:17 +0300

    feat: update the login form to use useFormik hook

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -2,35 +2,36 @@ import { images } from '@/assets'
 import { LoginForm, LoginPageContainer } from './styles'
 import { Button, Typography } from '@mui/material'
 import { useAppSelector } from '@/globalHooks';
-import { ChangeEvent, FormEvent, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
 export default function Login() {
-  const navigate = useNavigate()
-  const [formData, setformData] = useState({
-    email: '',
+  interface formInterface {
+    phoneNumber:string,
+    password:string,
+    keepLoggedIn:boolean
+}
+  const formData = {
+    phoneNumber: '',
     password:'',
     keepLoggedIn: false
+  }
+  const validationSchema=yup.object().shape({
+    phoneNumber: yup.string()
+    .matches(/^0[0-9]{9}$/, 'Phone number must start with 0 and have exactly 10 digits')
+    .required('Phone number is required'),
+    password: yup.string().required('Password is required'),
   })
+  const handleSubmit = (values:formInterface)=>{
+    console.log(values)
+  }
+  const formik = useFormik({
+    initialValues:formData,
+    validationSchema,
+    onSubmit:(values:formInterface)=>handleSubmit(values)
+  })
+
   const authState = useAppSelector(state=>state.auth)
-  const handleSubmit = (e:FormEvent)=>{
-    e.preventDefault()
-    console.log(formData)
-  }
-  const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
-    if(e.target.name==='keepLoggedIn'){
-      console.log(formData.keepLoggedIn)
-       setformData({
-        ...formData,
-        keepLoggedIn:(!formData.keepLoggedIn)
-      })}  
-    else setformData({
-      ...formData,
-      [e.target.name]:e.target.value
-    })
-    console.log(e.target.name)
-    console.log(e.target.value)
-  }
   return (
     <LoginPageContainer>
       <LoginForm >
@@ -40,26 +41,33 @@ export default function Login() {
         <Typography className='formTitle'>
           Landord Login
         </Typography>
-        <form className='form' onSubmit={handleSubmit} >
+        <form className='form' onSubmit={formik.handleSubmit} >
           <div className='formInput'>
-            <label htmlFor="email">Email</label>
-            <input value={formData.email}id='email' name='email' type='email'onChange={handleChange}/>
+            <label htmlFor="phoneNumber">Phone Number</label>
+            <input className='input' value={formik.values.phoneNumber}id='phoneNumber' name='phoneNumber' type='text'placeholder='e.g 0712345678'onBlur={formik.handleBlur}onChange={formik.handleChange}/>
+            {formik.touched.phoneNumber && formik.errors.phoneNumber &&
+              <Typography color='coral' fontSize='.9rem'>{formik.errors.phoneNumber}</Typography>
+            }
           </div>
           <div className='formInput'>
             <label htmlFor="password">Password</label>
-            <input value={formData.password} id='password' name='password' type='password'onChange={handleChange}/>
+            <input className='input' value={formik.values.password} id='password' name='password' type='password'onBlur={formik.handleBlur}onChange={formik.handleChange}/>
+            {formik.touched.password && formik.errors.password&&
+              <Typography color='coral' fontSize='.9rem'>{formik.errors.password}</Typography>
+            }
           </div>
+          <>
           {authState.error&&<Typography textAlign='center' margin='.75rem' color='red'>Invalid email or password</Typography>}
+          </>
           <div className='keepLoggedInSection'>
             <label>Keep me signed in</label>
               <input 
-              checked={formData.keepLoggedIn}
-              value={String(formData.keepLoggedIn)}
+              checked={formik.values.keepLoggedIn}
               type='checkbox'
-              onChange={handleChange}
+              onChange={formik.handleChange}
               name='keepLoggedIn' style={{marginLeft:'1rem'}}/>
           </div>
-          <Button className='button' type='submit' onClick={()=>navigate('/home')}>Sign In</Button>
+          <Button className='button' type='submit'>Sign In</Button>
         </form>
       </LoginForm>
     </LoginPageContainer>
