b52b360e7f896f6eb4357842794ca074bdc5a2be
Kangonga <guchu413@gmail.com>
2023-05-17 09:19:42 +0300

    feat: add, import and use ai image as profile photo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,7 @@
 import  logo  from './images/logo.png'
+import profile from '@/assets/images/aiFace.jpg'
 
 export const images = {
-    logo
+    logo,
+    profile
 }
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,12 +1,13 @@
 import { Avatar, Divider, Typography } from "@mui/material";
 import { Form, ProfilePageContainer, ProfileTopSection } from "./styles";
+import { images } from "@/assets";
 
 export default function Profile() {
   return (
     <ProfilePageContainer>
       <ProfileTopSection>
         <section className="avatar">
-          <Avatar sx={{width:'150px', height:'150px'}}/>
+          <Avatar sx={{width:'150px', height:'150px'}} src={images.profile}/>
         </section>
         <section className="nameContainer">
           <span className="name">John Doe</span>
@@ -18,15 +19,15 @@ export default function Profile() {
         <Divider />
         <div>
           <label htmlFor="Full name">Full name</label>
-          <input type="text" />
+          <input type="text" disabled value={'John Doe'}/>
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" />
+          <input type="text" disabled value={'jdoe@mail.com'}/>
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" />
+          <input type="password" disabled value={'*******'}/>
         </div>
       </Form>
 
