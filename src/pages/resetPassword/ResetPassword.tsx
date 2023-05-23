257adaa4eeae084731f5b68ddbef7e9a98a46f9b
Kangonga <guchu413@gmail.com>
2023-05-23 14:41:16 +0300

    feat: implement reset password functionality

--- src/pages/resetPassword/ResetPassword.tsx
+++ src/pages/resetPassword/ResetPassword.tsx
@@ -1,19 +1,29 @@
 import { FormEvent, useState } from 'react'
-import { LoginForm, LoginPageContainer } from '../login/styles'
-import { images } from '@/assets'
-import { Button, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom';
+import useUpdatePassword from '@/hooks/useUpdatePassword';
+import { useAppSelector } from '@/globalHooks';
+import { LoginForm, LoginPageContainer } from '../login/styles';
+import { images } from '@/assets';
+import { Button, Typography } from '@mui/material';
 
 export default function ResetPassword() {
+    const state = useAppSelector(state=>state.auth)
+    const token = useAppSelector(state=>state.auth.token)
+    const userId = useAppSelector(state=>state.auth.userId)
+    console.log('token and id',token,userId)
+    console.log('state of the app', state )
     const navigate = useNavigate()
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
+    const [isPasswordChanged, setIsPasswordChanged] = useState(false)
     const handleSubmit = (e:FormEvent)=>{
         e.preventDefault()
-        if (confirmPassword===password){
-            navigate('/')
-        }
-        console.log(password)
+        setIsPasswordChanged(true)
+        if (confirmPassword===password){(async()=>{
+            const response = await useUpdatePassword(userId,password,token)
+            console.log('after password update', response)
+            navigate('/') 
+        })}
     }
     return (
     <LoginPageContainer>
@@ -28,15 +38,15 @@ export default function ResetPassword() {
             <form onSubmit={(e:FormEvent)=>handleSubmit(e)} className='form'>
                <div className='formInput'>
                 <label htmlFor="password" >Enter New Password</label>
-                <input className='input' name="password"value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <input disabled={isPasswordChanged} className='input' name="password"value={password} onChange={(e)=>setPassword(e.target.value)}/>
                </div>
                <div className='formInput'>
                 <label htmlFor="confirmPassword" >Confirm Password</label>
-                <input className='input' name="confirmPassword"value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
+                <input disabled={isPasswordChanged} className='input' name="confirmPassword"value={confirmPassword} onChange={(e)=>setConfirmPassword(e.target.value)}/>
                </div>
                {!(confirmPassword===password)&&
                <div  style={{textAlign:'center',color:'coral', paddingBottom:'.75rem'}}>passwords do not match</div>}
-               <Button className='button' type='submit'>Sign In</Button>
+               <Button disabled={isPasswordChanged} className='button' type='submit'>Sign In</Button>
             </form>
         </LoginForm>
     </LoginPageContainer>
