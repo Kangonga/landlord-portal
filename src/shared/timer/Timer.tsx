3a42ab4573cde8714403bb8ecbee475eddf17b5d
Kangonga <guchu413@gmail.com>
2023-05-23 22:22:19 +0300

    feat: update timer to include resend code functionality

--- src/shared/timer/Timer.tsx
+++ src/shared/timer/Timer.tsx
@@ -21,17 +21,39 @@
 //         </div>
 //     )
 // }
-import { Box, Button } from '@mui/material';
-import React, { useState } from 'react';
+import { authActions } from '@/features/authentication/authSlice';
+import { useAppDispatch, useAppSelector } from '@/globalHooks';
+import useResetPassword from '@/hooks/useResetPassword';
+import { LoginButtonContainer } from '@/pages/login/styles';
+import {  Button } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export const ResendCodeButton: React.FC = () => {
+interface timerProps {
+  setShowError(val:boolean):any
+}
+export function ResendCodeButton ({setShowError}:timerProps) {
+  const navigate = useNavigate()
+  const dispatch = useAppDispatch()
   const [disabled, setDisabled] = useState(false);
   const [timer, setTimer] = useState(0);
-
+  const [isRequestSent, setIsRequestSent] = useState(false)
+  const phone = useAppSelector(state=>state.auth.phone)
   const handleClick = () => {
     setDisabled(true);
-    setTimer(10);
-
+    setTimer(20);
+    setIsRequestSent(true)
+    const resendCode = async()=>{
+      const response = await useResetPassword(phone)
+      if(response.status==0){
+        console.log('response type',typeof(response.status))
+        dispatch(authActions.updateUserId(response.data.user))
+      }
+      else{
+        setShowError(true)
+      }
+    }
+    resendCode()
     const countdown = setInterval(() => {
       setTimer(prevTimer => prevTimer - 1);
     }, 1000);
@@ -39,16 +61,17 @@ export const ResendCodeButton: React.FC = () => {
     setTimeout(() => {
       setDisabled(false);
       clearInterval(countdown);
-    }, 10000);
+      setIsRequestSent(false)
+    }, 20000);
   };
 
   return (
-    <Box sx={{display:'flex', gap:'.75rem',alignItems:'center'}}>
-      <Button onClick={handleClick} disabled={disabled}>
+    <LoginButtonContainer isRequestSent={isRequestSent}>
+      <Button onClick={handleClick} disabled={disabled} sx={{color:'white'}}>
         Resend Code
       </Button>
       {timer > 0 && <span style={{color:'darkgray'}}>{timer} seconds</span>}
-    </Box>
+    </LoginButtonContainer>
   );
 };
 
