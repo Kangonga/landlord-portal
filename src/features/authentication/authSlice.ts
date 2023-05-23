0e9983dc9cf2c9bba2cf886cad38df5e5b100f6a
Kangonga <guchu413@gmail.com>
2023-05-23 14:36:07 +0300

    feat: revamp the authslice to work without thunks

--- src/features/authentication/authSlice.ts
+++ src/features/authentication/authSlice.ts
@@ -1,50 +1,31 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
 
 export interface authInterface {
-    loading: boolean,
-    userInfo: {},
-    userToken: string,
-    error: string|undefined,
-    isLoggedIn: boolean,
+    userId:string,
+    token:string
 }
 const initialState:authInterface = {
-    loading: false,
-    userInfo: {},
-    userToken: '',
-    error: '',
-    isLoggedIn: false,
+    userId: '',
+    token: ''
 }
 
-const login = createAsyncThunk('auth/login',async ()=>{
-    const response = await axios.post('api/login');
-    return response.data;
-})
+
 
 const authReducer = createSlice({
     name:'auth',
     initialState,
     reducers:{
+        updatePassword:(state,action)=>{
+            console.log('payload', action)
+        },
+        login:(state, action)=>{
+            state.userId = action.payload.user
+            state.token = action.payload.token
+            console.log('token updated',state.token)
+        },
         logout: ()=> initialState
-    },
-    extraReducers: builder=>{
-        builder
-        .addCase(login.pending, state=>{
-            state.loading = true
-        })
-        .addCase(login.fulfilled, (state, action)=>{
-            state.loading = false
-            state.userInfo = action.payload
-            state.isLoggedIn = true
-            state.userToken = action.payload.userToken
-        })
-        .addCase(login.rejected, (state, action)=>{
-            state.loading = false
-            state.error = action.error.message
-        } )
     }
 })
 
 export default authReducer.reducer
 export const authActions = authReducer.actions
-export { login }
