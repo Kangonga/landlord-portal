cf287a13954d2d50bee1cce84ce2a1ae445d5d1b
Kangonga <guchu413@gmail.com>
2023-05-23 14:39:11 +0300

    feat: implement login functionality

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,39 +1,64 @@
 import { images } from '@/assets'
 import { ForgotPassword, LoginForm, LoginPageContainer } from './styles'
 import { Button, Typography } from '@mui/material'
-import { useAppSelector } from '@/globalHooks';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import useLogin from '@/hooks/useLogin';
+import { useState } from 'react';
+import { useAppDispatch } from '@/globalHooks';
+import { authActions } from '@/features/authentication/authSlice';
 
 export default function Login() {
+  const dispatch = useAppDispatch()
+  const [isError, setIsError] =  useState(false)
+  const [isRequestSent,setIsRequestSent] =  useState(false)
+
   const navigate = useNavigate()
   interface formInterface {
-    phoneNumber:string,
+    phone:string,
     password:string,
     keepLoggedIn:boolean
 }
+interface apiResult {
+  data:{
+      user:number,
+      token:string
+  },
+  status:number,
+  statusDesc:string
+}
   const formData = {
-    phoneNumber: '',
+    phone: '',
     password:'',
     keepLoggedIn: false
   }
   const validationSchema=yup.object().shape({
-    phoneNumber: yup.string()
+    phone: yup.string()
     .matches(/^0[0-9]{9}$/, 'Phone number must start with 0 and have exactly 10 digits')
     .required('Phone number is required'),
     password: yup.string().required('Password is required'),
   })
-  const handleSubmit = (values:formInterface)=>{
-    console.log(values)
+  const handleSubmit = async (values:formInterface)=>{
+    setIsRequestSent(true)
+    const response:apiResult = await useLogin({phone:values.phone, password:values.password})
+    // console.log(response)
+    if(response.status===1){
+      setIsError(true)
+      setIsRequestSent(false)
+    }
+    else{
+      const user = response.data.user
+      const token = response.data.token
+      setIsError(false)
+      dispatch(authActions.login({user, token}))
+    }
   }
   const formik = useFormik({
     initialValues:formData,
     validationSchema,
-    onSubmit:(values:formInterface)=>handleSubmit(values)
+    onSubmit:async (values:formInterface)=>await handleSubmit(values)
   })
-
-  const authState = useAppSelector(state=>state.auth)
   return (
     <LoginPageContainer>
       <LoginForm >
@@ -45,21 +70,21 @@ export default function Login() {
         </Typography>
         <form className='form' onSubmit={formik.handleSubmit} >
           <div className='formInput'>
-            <label htmlFor="phoneNumber">Phone Number</label>
-            <input className='input' value={formik.values.phoneNumber}id='phoneNumber' name='phoneNumber' type='text'placeholder='e.g 0712345678'onBlur={formik.handleBlur}onChange={formik.handleChange}/>
-            {formik.touched.phoneNumber && formik.errors.phoneNumber &&
-              <Typography color='coral' fontSize='.9rem'>{formik.errors.phoneNumber}</Typography>
+            <label htmlFor="phone">Phone Number</label>
+            <input disabled={isRequestSent} className='input' value={formik.values.phone}id='phone' name='phone' type='text'placeholder='e.g 0712345678'onBlur={formik.handleBlur}onChange={formik.handleChange}/>
+            {formik.touched.phone && formik.errors.phone &&
+              <Typography color='coral' fontSize='.9rem'>{formik.errors.phone}</Typography>
             }
           </div>
           <div className='formInput'>
             <label htmlFor="password">Password</label>
-            <input className='input' value={formik.values.password} id='password' name='password' type='password'onBlur={formik.handleBlur}onChange={formik.handleChange}/>
+            <input disabled={isRequestSent} className='input' value={formik.values.password} id='password' name='password' type='password'onBlur={formik.handleBlur}onChange={formik.handleChange}/>
             {formik.touched.password && formik.errors.password&&
               <Typography color='coral' fontSize='.9rem'>{formik.errors.password}</Typography>
             }
           </div>
           <>
-          {authState.error&&<Typography textAlign='center' margin='.75rem' color='red'>Invalid email or password</Typography>}
+          {isError&&<Typography textAlign='center' margin='.75rem' color='red'>Invalid email or password</Typography>}
           </>
           <div className='keepLoggedInSection'>
             <label>Keep me signed in</label>
@@ -69,7 +94,7 @@ export default function Login() {
               onChange={formik.handleChange}
               name='keepLoggedIn' style={{marginLeft:'1rem'}}/>
           </div>
-          <Button className='button' type='submit'>Sign In</Button>
+          <Button disabled={isRequestSent} className='button' type='submit'>Sign In</Button>
         </form>
         <ForgotPassword className='forgotPassword'>
             <Button onClick={()=>navigate('/forgotPassword')}>Forgot password? Click to reset</Button>
