1209b89a5baa98d3de6bb4d4c4f6582d393f8b33
Kangonga <guchu413@gmail.com>
2023-06-27 15:59:01 +0300

    feat: changed login endpoint

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,20 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
-interface loginProps{
-    phone: string,
-    password:string
+interface loginProps {
+  phone: string;
+  password: string;
 }
-export default async function useLogin(props:loginProps) {
-    const url = 'https://enerlytics.m-paya.net/user?'
-    const params = {
-        api:{
-            request:'login'
-        },
-        input:{
-            phone:props.phone,
-            password:props.password
-        }
-    }
-    const result = await axios.post(url, params)
-    return result.data
+export default async function useLogin(props: loginProps) {
+  const url = "https://ss.m-paya.net/api.php?src=login";
+  const params = {
+    api: {
+      request: "login",
+    },
+    input: {
+      phone: props.phone,
+      password: props.password,
+    },
+  };
+  const result = await axios.post(url, params);
+  console.log(result.data);
+  return result.data;
 }
