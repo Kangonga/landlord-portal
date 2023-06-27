dc7ac251e4f385d85b0f48c2bd4fc2c0866d1a7d
Kangonga <guchu413@gmail.com>
2023-06-27 15:59:49 +0300

    refactor: format code to follow prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Sidebar from "@/shared/sidebar/Sidebar" 
-import { ComponentContainer, Container } from "./shared/components/sharedStyles"
-import { Outlet } from "react-router-dom"
+import Sidebar from "@/shared/sidebar/Sidebar";
+import {
+  ComponentContainer,
+  Container,
+} from "./shared/components/sharedStyles";
+import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <Container>
-        <Sidebar />
-        <ComponentContainer>
-            <Outlet />
-        </ComponentContainer>
+      <Sidebar />
+      <ComponentContainer>
+        <Outlet />
+      </ComponentContainer>
     </Container>
-  )
+  );
 }
--- src/features/buildingFeatures/components/buildingBarChart/buildingBarChartDataStructure.tsx
+++ src/features/buildingFeatures/components/buildingBarChart/buildingBarChartDataStructure.tsx
@@ -1,41 +1,51 @@
-
-export function getLast12Months(){
-    let monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let d = new Date();
-    let months = []
-    for (let i=0; i<=11; i++) {
-        let year = String(d.getFullYear()).slice(-2)
-        let month = monthName[d.getMonth()] + ' / ' + year;
-        months.push(month)
-        d.setMonth(d.getMonth() - 1);
-    }
-    return {
-        start:`${months[0]}`,
-        end: `${months[11]}`,
-        labels: `${months}`
-    }
+export function getLast12Months() {
+  const monthName = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const d = new Date();
+  const months = [];
+  for (let i = 0; i <= 11; i++) {
+    const year = String(d.getFullYear()).slice(-2);
+    const month = monthName[d.getMonth()] + " / " + year;
+    months.push(month);
+    d.setMonth(d.getMonth() - 1);
+  }
+  return {
+    start: `${months[0]}`,
+    end: `${months[11]}`,
+    labels: `${months}`,
+  };
 }
 //obtain labels for last 12 months
 //make api request with the start and end dates
 
 //bar chart options
-const barchartTitle: string = 'Building collections for the last 12 months'
-const barChartLabels = getLast12Months().labels
+const barchartTitle = "Building collections for the last 12 months";
+const barChartLabels = getLast12Months().labels;
 export const options = {
-    responsive:true,
-    plugins: {
-        legend: {
-          position: 'top'
-        },
-        title: {
-          display: true,
-          text: barchartTitle,
-        },
-      },
-}
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: true,
+      text: barchartTitle,
+    },
+  },
+};
 export const barChartData = {
-    labels: barChartLabels,
-    datasets:[
-
-    ]
-}
+  labels: barChartLabels,
+  datasets: [],
+};
