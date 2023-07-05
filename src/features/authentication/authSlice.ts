90a206502ee14d7f4a9d177ccf7a8adffc38161b
Kangonga <guchu413@gmail.com>
2023-07-05 06:06:08 +0300

    feat: create authentication slice

--- src/features/authentication/authSlice.ts
+++ src/features/authentication/authSlice.ts
@@ -1,45 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface authInterface {
-    userId:string,
-    token:string,
-    phone:string
+  userId: string;
+  token: string;
+  phone: string;
+  names: string;
+  phone1: number | null;
+  phone2: number | null;
+  email: string | null;
+  address: string | null;
+  nationalID: string | null;
+  regDate: string;
 }
-const initialState:authInterface = {
-    userId: '',
-    token: '',
-    phone:''
-}
-
-
+const initialState: authInterface = {
+  userId: "",
+  token: "",
+  phone: "",
+  names: "",
+  phone1: null,
+  phone2: null,
+  email: "",
+  address: "",
+  nationalID: "",
+  regDate: "",
+};
 
 const authReducer = createSlice({
-    name:'auth',
-    initialState,
-    reducers:{
-        updatePhoneNumber:(state,action)=>{
-        state.phone = action.payload
-        console.log('phone updated')
-        },
-        updateUserId:(state,action)=>{
-            console.log('payload', action.payload)
-            state.userId = action.payload
-            console.log('userid updated', state.userId)
-        },
-        updateToken:(state,action)=>{
-            state.token = action.payload.token
-        },
-        updatePassword:(action)=>{
-            console.log('payload', action)
-        },
-        login:(state, action)=>{
-            state.userId = action.payload.user
-            state.token = action.payload.token
-            console.log('token updated',state.token)
-        },
-        logout: ()=> initialState
-    }
-})
+  name: "auth",
+  initialState,
+  reducers: {
+    updatePhoneNumber: (state, action) => {
+      state.phone = action.payload;
+    },
+    updateUserId: (state, action) => {
+      console.log("payload", action.payload);
+      state.userId = action.payload;
+    },
+    updateToken: (state, action) => {
+      state.token = action.payload.token;
+    },
+    updatePassword: (action) => {
+      console.log("payload", action);
+    },
+    login: (state, action) => {
+      state.userId = action.payload.user;
+      state.token = action.payload.token;
+    },
+    logout: () => initialState,
+  },
+});
 
-export default authReducer.reducer
-export const authActions = authReducer.actions
+export default authReducer.reducer;
+export const authActions = authReducer.actions;
