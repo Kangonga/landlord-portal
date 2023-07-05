23b86f14f3607c4c0faacdeb445b507431043175
Kangonga <guchu413@gmail.com>
2023-07-05 06:05:19 +0300

    fix: remove dependence on building utility

--- src/features/buildingFeatures/components/widgets/BuildingWidgets.tsx
+++ src/features/buildingFeatures/components/widgets/BuildingWidgets.tsx
@@ -1,23 +1,21 @@
-import { useAppSelector } from '@/globalHooks'
-import { BuildingWidget, WidgetsContainer } from './styles'
+import { useAppSelector } from "@/globalHooks";
+import { BuildingWidget, WidgetsContainer } from "./styles";
 
 export default function BuildingWidgets() {
-const activeBuildingDetails = useAppSelector(state=>state.utility.activeBuilding)
-console.log('active building', activeBuildingDetails)
   return (
-        <WidgetsContainer>
-            <BuildingWidget>
-                    <span id='clientLabel'>Client:</span>
-                    <span id='clientName'>John Doe</span>
-            </BuildingWidget>
-            <BuildingWidget>
-                    <span id='clientLabel'>Building Name:</span>
-                    <span id='clientName'>{activeBuildingDetails.label}</span>
-            </BuildingWidget>
-            <BuildingWidget>
-                    <span id='clientLabel'>Building location:</span>
-                    <span id='clientName'>Makongeni, Thika</span>
-            </BuildingWidget>
-        </WidgetsContainer>
-   )
+    <WidgetsContainer>
+      <BuildingWidget>
+        <span id="clientLabel">Client:</span>
+        <span id="clientName">John Doe</span>
+      </BuildingWidget>
+      <BuildingWidget>
+        <span id="clientLabel">Building Name:</span>
+        <span id="clientName">{}</span>
+      </BuildingWidget>
+      <BuildingWidget>
+        <span id="clientLabel">Building location:</span>
+        <span id="clientName">Makongeni, Thika</span>
+      </BuildingWidget>
+    </WidgetsContainer>
+  );
 }
--- src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
+++ src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
@@ -1,53 +1,46 @@
-import { useAppSelector } from '@/globalHooks'
-import { MeterWidget, WidgetsContainer } from './styles'
-import { Autocomplete, TextField } from '@mui/material'
+import { useAppSelector } from "@/globalHooks";
+import { MeterWidget, WidgetsContainer } from "./styles";
+import { Autocomplete, TextField } from "@mui/material";
 
 export default function MeterWidgets() {
-const activeBuildingDetails = useAppSelector(state=>state.utility.activeBuilding)
-console.log('active building', activeBuildingDetails)
   return (
-        <WidgetsContainer>
-            <MeterWidget>
-                    <span id='clientLabel'>Client:</span>
-                    <span id='clientName'>John Doe</span>
-            </MeterWidget>
-            <MeterWidget>
-                    <span id='clientLabel'>Building Name:</span>
-                    <span id='clientName'>{activeBuildingDetails.label}</span>
-            </MeterWidget>
-            <Autocomplete
-                disablePortal
-                sx={{ width:'200px' }}
-                id="combo-box"
-                options={ ['elec','water','all meters'] }
-                renderInput={(params) => 
-                <TextField {...params} label='Select utility'
-                />
-                }
-                noOptionsText='No meter found'
-           />
-            <Autocomplete
-                disablePortal
-                sx={{ width:'200px' }}
-                id="combo-box"
-                options={ ['123134421','873243432','all meters'] }
-                renderInput={(params) => 
-                <TextField {...params} label='Select meter'
-                />
-                }
-                noOptionsText='No meter found'
-           />
-           <Autocomplete
-                disablePortal
-                sx={{ width:'200px' }}
-                id="combo-box"
-                options={ ['123134421','873243432','all m-paya meters'] }
-                renderInput={(params) => 
-                <TextField {...params} label='Select m-paya meter'
-                />
-                }
-                noOptionsText='No meter found'
-           />
-        </WidgetsContainer>
-   )
+    <WidgetsContainer>
+      <MeterWidget>
+        <span id="clientLabel">Client:</span>
+        <span id="clientName">John Doe</span>
+      </MeterWidget>
+      <MeterWidget>
+        <span id="clientLabel">Building Name:</span>
+        <span id="clientName">{}</span>
+      </MeterWidget>
+      <Autocomplete
+        disablePortal
+        sx={{ width: "200px" }}
+        id="combo-box"
+        options={["elec", "water", "all meters"]}
+        renderInput={(params) => (
+          <TextField {...params} label="Select utility" />
+        )}
+        noOptionsText="No meter found"
+      />
+      <Autocomplete
+        disablePortal
+        sx={{ width: "200px" }}
+        id="combo-box"
+        options={["123134421", "873243432", "all meters"]}
+        renderInput={(params) => <TextField {...params} label="Select meter" />}
+        noOptionsText="No meter found"
+      />
+      <Autocomplete
+        disablePortal
+        sx={{ width: "200px" }}
+        id="combo-box"
+        options={["123134421", "873243432", "all m-paya meters"]}
+        renderInput={(params) => (
+          <TextField {...params} label="Select m-paya meter" />
+        )}
+        noOptionsText="No meter found"
+      />
+    </WidgetsContainer>
+  );
 }
