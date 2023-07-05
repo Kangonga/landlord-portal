096a02cd9d69a64dea7e8b851a2cd393ff4193eb
Kangonga <guchu413@gmail.com>
2023-07-05 06:01:57 +0300

    fix: remove redundant state

--- src/shared/topbar/Topbar.tsx
+++ src/shared/topbar/Topbar.tsx
@@ -1,42 +1,41 @@
-import { Autocomplete, Avatar, TextField } from '@mui/material'
-import { AvatarContainer, StyledBadge, TopBarContainer } from './styles'
-import { useAppDispatch, useAppSelector } from '@/globalHooks'
-import { utilityActions } from '@/features/utilitySlice'
+import { Autocomplete, Avatar, TextField } from "@mui/material";
+import { AvatarContainer, StyledBadge, TopBarContainer } from "./styles";
+import { useAppDispatch, useAppSelector } from "@/globalHooks";
 
 function Topbar() {
-  const buildings = useAppSelector(state=>state.utility.allBuildings)
-  const utilityState = useAppSelector(state=>state.utility)
-  const dispatch = useAppDispatch()
+  // const buildings = useAppSelector((state) => state.utility.allBuildings);
+  // const utilityState = useAppSelector((state) => state.utility);
+  const dispatch = useAppDispatch();
   return (
     <TopBarContainer>
       <Autocomplete
         disablePortal
-        sx={{ width:'200px' }}
+        sx={{ width: "200px" }}
         id="combo-box"
-        options={ buildings }
-        renderInput={(params) => 
-          <TextField {...params} label='Select Building'
-          />
-        }
-        noOptionsText='No buildings found'
-        value={utilityState.activeBuilding}
-        onChange={(_e, newValue)=>dispatch(utilityActions.updateBuilding(newValue))}
+        options={["building one"]}
+        renderInput={(params) => (
+          <TextField {...params} label="Select Building" />
+        )}
+        noOptionsText="No buildings found"
+        // onChange={(_e, newValue) =>
+        //   dispatch(utilityActions.updateBuilding(newValue))
+        // }
       />
       <AvatarContainer>
         <StyledBadge
           overlap="circular"
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
           variant="dot"
         >
           <Avatar alt="Landlord image" src="" />
-       </StyledBadge>
-         <article>
-          <span id='name'>John Doe</span>
-          <span id='email'>jdoe@mail.com</span>
+        </StyledBadge>
+        <article>
+          <span id="name">John Doe</span>
+          <span id="email">jdoe@mail.com</span>
         </article>
       </AvatarContainer>
     </TopBarContainer>
-  )
+  );
 }
 
-export default Topbar
+export default Topbar;
