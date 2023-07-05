17b6e160e61aafc1eab4a7913402cb02bc3f37f4
Kangonga <guchu413@gmail.com>
2023-07-05 06:04:51 +0300

    refactor: use props rather than hard coded values

--- src/hooks/getDashboardData.ts
+++ src/hooks/getDashboardData.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const getDashboardData = async (token: string) => {
+export const getDashboardData = async (token: string, user: string) => {
   const url = "https://ss.m-paya.net/api.php?src=dashboard";
   const params = {
     api: {
       request: "viewAccount",
-      user: "2012071742101762",
-      token: token,
+      user,
+      token,
     },
   };
   const response = await axios.post(url, params);
