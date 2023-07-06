7114454cef29866fde4dca423dcf7572197c9c5f
Kangonga <guchu413@gmail.com>
2023-07-06 13:26:41 +0300

    refactor: cleanup the file

--- src/pages/mainMeter/MainMeter.tsx
+++ src/pages/mainMeter/MainMeter.tsx
@@ -1,23 +1,25 @@
-import { Box, Typography } from '@mui/material'
-import MeterDataTables from '../../features/mainMeterFeatures/components/mainMeterDataTable/MeterDataTables'
-import MeterWidgets from '../../features/mainMeterFeatures/components/widgets/MeterWidgets'
-import { MainMeterContainer } from './styles'
-import { Bar, BarChartContainer } from '@/features/buildingFeatures/pages/styles'
-import Topbar from '@/shared/topbar/Topbar'
+import { Box, Typography } from "@mui/material";
+import MeterDataTables from "../../features/mainMeterFeatures/components/mainMeterDataTable/MeterDataTables";
+import MeterWidgets from "../../features/mainMeterFeatures/components/widgets/MeterWidgets";
+import { MainMeterContainer } from "./styles";
+import { BarChartContainer } from "@/features/buildingFeatures/pages/styles";
+import Topbar from "@/shared/topbar/Topbar";
+import { Barchart } from "@/features/buildingFeatures/components/buildingBarChart/buildingBarChart";
 
 export default function MainMeter() {
   return (
     <Box>
       <Topbar />
-       <MainMeterContainer>
+      <MainMeterContainer>
         <MeterWidgets />
         <BarChartContainer>
-            <Bar />
+          <Barchart />
         </BarChartContainer>
-        <Typography fontSize='1.3rem' color='blue' textAlign='center'>Meter Details</Typography>
+        <Typography fontSize="1.3rem" color="blue" textAlign="center">
+          Meter Details
+        </Typography>
         <MeterDataTables />
-    </MainMeterContainer>
+      </MainMeterContainer>
     </Box>
-   
-  )
+  );
 }
