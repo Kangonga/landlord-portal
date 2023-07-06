ca6ef32eaffc6faee3593293df6956b1b3a476d5
Kangonga <guchu413@gmail.com>
2023-07-06 08:20:47 +0300

    feat: update data table display

--- src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
+++ src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
@@ -1,23 +1,33 @@
-import { Box, Typography } from '@mui/material'
-import { DataGrid, GridToolbar } from '@mui/x-data-grid'
-import { columns, rows } from './buildingTableDataStructure'
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { columns } from "./buildingTableDataStructure";
+import { summaryInterface } from "@/hooks/getUtilitySummary";
 
-export default function BuildingDataTable() {
+export default function BuildingDataTable({
+  rows,
+}: {
+  rows: summaryInterface[];
+}) {
   return (
     <Box>
-      <Typography sx={{fontSize:'1.2rem', textAlign:'center', color:'gray'}}>Building summary</Typography>
-        <DataGrid 
-            columns={columns}
-            rows={rows}
-            slots={{
-                toolbar:GridToolbar
-              }}
-            sx={{
-                height:'max-content',
-                overflow:'auto',
-                paddingBottom:'1rem'
-            }}
-        />
+      <Typography
+        sx={{ fontSize: "1.2rem", textAlign: "center", color: "gray" }}
+      >
+        July Building summary
+      </Typography>
+      <>{console.log("rows", rows)}</>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        sx={{
+          height: "max-content",
+          overflow: "auto",
+          paddingBottom: "1rem",
+        }}
+      />
     </Box>
-  )
+  );
 }
