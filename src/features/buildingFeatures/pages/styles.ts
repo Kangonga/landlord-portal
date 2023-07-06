f3c2d1ba38d35670ac4ab2823b2c9ab5f3bcd5ae
Kangonga <guchu413@gmail.com>
2023-07-06 08:18:41 +0300

    refactor: format document

--- src/features/buildingFeatures/pages/styles.ts
+++ src/features/buildingFeatures/pages/styles.ts
@@ -1,23 +1,23 @@
 import { styled } from "@mui/material";
 
-export const BarChartContainer = styled('div')(({ theme })=>({
-    display:'flex',
-    gap:'.4rem',
-    flexWrap:'wrap',
-    [(theme.breakpoints.down('sm'))]:{
-        flexDirection:'column',
-    },
-    '#barchart':{
-        minWidth:"600px"
-    }
-}))
+export const BarChartContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  gap: ".4rem",
+  flexWrap: "wrap",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+  "#barchart": {
+    minWidth: "600px",
+  },
+}));
 
-export const Bar = styled('div')(({ theme })=>({
-    height:'500px',
-    width:'700px',
-    minWidth:'500px',
-    backgroundColor:'gray',
-    [( theme.breakpoints.down('lg' ))]:{
-        width:'100%'
-    },
-}))
+export const Bar = styled("div")(({ theme }) => ({
+  height: "500px",
+  width: "700px",
+  minWidth: "500px",
+  // backgroundColor:'',
+  [theme.breakpoints.down("lg")]: {
+    width: "100%",
+  },
+}));
