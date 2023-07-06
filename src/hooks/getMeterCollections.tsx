1957b444591dc5a2e67a181cddffa7cd226f2a1f
Kangonga <guchu413@gmail.com>
2023-07-06 21:21:44 +0300

    feat: add hook to get sub meter collections

--- src/hooks/getMeterCollections.tsx
+++ src/hooks/getMeterCollections.tsx
@@ -1,22 +1,14 @@
-import { getLast12Months } from "@/features/buildingFeatures/components/buildingBarChart/buildingBarChartDataStructure";
-import { motherMeterInterface } from "@/pages/login/interfaces";
+import { motherMeterSliceInterface } from "@/features/mainMeterFeatures/meterSlice";
+import { subMeterInterface } from "@/pages/login/interfaces";
 
 export default function useGetMeterCollections(
   currentMonth: string,
   currentYear: string,
-  mm: motherMeterInterface
+  mm: motherMeterSliceInterface
 ) {
   const paymentMonth = currentMonth.slice(0, 3);
   let collections = 0;
-  // const last12months = getLast12Months().months;
-  // const current = last12months[0].trim().split("/")[0];
-  // console.log(current);
   const sm = mm.sm;
-  // console.log("inside get collections");
-  // console.log("month", paymentMonth);
-  // console.log("year", currentYear);
-  // console.log("sub meters", sm);
-
   for (const submeter of sm) {
     const payments = submeter.payments;
     if (submeter.payments) {
@@ -24,11 +16,27 @@ export default function useGetMeterCollections(
         const month = payment.payTime?.split(" ")[1];
         const year = payment.payTime?.split(" ")[2];
         if (month == paymentMonth && year == currentYear) {
-          // console.log("payment equal", payment);
           collections = collections + Number(payment.amount);
-          // console.log("collections", collections);
-          // console.log("month", month, "paymentMonth", paymentMonth);
         }
+      }
+    }
+  }
+  return collections;
+}
+
+export function useGetSubMeterCollections(
+  currentMonth: string,
+  currentYear: string,
+  sm: subMeterInterface
+) {
+  const paymentMonth = currentMonth.slice(0, 3);
+  let collections = 0;
+  if (sm.payments) {
+    for (const payment of sm.payments) {
+      const month = payment.payTime?.split(" ")[1];
+      const year = payment.payTime?.split(" ")[2];
+      if (month == paymentMonth && year == currentYear) {
+        collections = collections + Number(payment.amount);
       }
     }
   }
