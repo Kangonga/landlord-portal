d20720bfa22bc5c077e9fe8bc3c660ae2df7b9b6
Kangonga <guchu413@gmail.com>
2023-07-06 13:26:17 +0300

    feat: clean up the function

--- src/features/buildingFeatures/components/buildingBarChart/buildingBarChart.tsx
+++ src/features/buildingFeatures/components/buildingBarChart/buildingBarChart.tsx
@@ -10,11 +10,9 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-// import * as faker from "faker";
 import { getLast12Months } from "@/features/buildingFeatures/components/buildingBarChart/buildingBarChartDataStructure";
 import useGetMeterCollections from "@/hooks/getMeterCollections";
 import { useAppSelector } from "@/globalHooks";
-// import faker from "@faker-js/faker";
 
 ChartJS.register(
   CategoryScale,
@@ -25,22 +23,22 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-    title: {
-      display: true,
-      text: "Last 12 months meter collections",
-    },
-  },
-};
-
 const labels = getLast12Months().months;
 
 export function Barchart() {
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Last 12 months meter collections",
+      },
+    },
+  };
+
   const motherMeters = useAppSelector((state) => state.utility.data.mm);
   const barData = [];
   for (const month of labels) {
@@ -49,9 +47,6 @@ export function Barchart() {
     const year = String(month.split("/")[1].trim());
     for (const mm of motherMeters) {
       const collections = useGetMeterCollections(currentMonth, year, mm);
-      //   console.log("collections for mothermeter", mm);
-      //   console.log("collections for period", currentMonth, year);
-      //   console.log("collections total value is", collections);
       monthlyCollections += collections;
     }
     barData.push({
