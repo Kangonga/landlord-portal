5ffc27cf4898310e8cda1402ebad83ffd8847178
Kangonga <guchu413@gmail.com>
2023-07-06 08:16:26 +0300

    refactor: update building widgets

--- src/features/buildingFeatures/components/widgets/BuildingWidgets.tsx
+++ src/features/buildingFeatures/components/widgets/BuildingWidgets.tsx
@@ -2,19 +2,20 @@ import { useAppSelector } from "@/globalHooks";
 import { BuildingWidget, WidgetsContainer } from "./styles";
 
 export default function BuildingWidgets() {
+  const utilityState = useAppSelector((state) => state.utility.data);
   return (
     <WidgetsContainer>
       <BuildingWidget>
         <span id="clientLabel">Client:</span>
-        <span id="clientName">John Doe</span>
+        <span id="clientName">{utilityState.Names}</span>
       </BuildingWidget>
       <BuildingWidget>
         <span id="clientLabel">Building Name:</span>
-        <span id="clientName">{}</span>
+        <span id="clientName">B1</span>
       </BuildingWidget>
       <BuildingWidget>
         <span id="clientLabel">Building location:</span>
-        <span id="clientName">Makongeni, Thika</span>
+        <span id="clientName">Not set</span>
       </BuildingWidget>
     </WidgetsContainer>
   );
