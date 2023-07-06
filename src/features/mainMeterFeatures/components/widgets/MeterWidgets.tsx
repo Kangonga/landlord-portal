e595921df727125adcc7b76cda26e1c6c613d030
Kangonga <guchu413@gmail.com>
2023-07-06 08:53:16 +0300

    feat: update displayed meter widget details

--- src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
+++ src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
@@ -3,21 +3,22 @@ import { MeterWidget, WidgetsContainer } from "./styles";
 import { Autocomplete, TextField } from "@mui/material";
 
 export default function MeterWidgets() {
+  const utilityState = useAppSelector((state) => state.utility.data);
   return (
     <WidgetsContainer>
       <MeterWidget>
         <span id="clientLabel">Client:</span>
-        <span id="clientName">John Doe</span>
+        <span id="clientName">{utilityState.Names}</span>
       </MeterWidget>
       <MeterWidget>
         <span id="clientLabel">Building Name:</span>
-        <span id="clientName">{}</span>
+        <span id="clientName">{"B1"}</span>
       </MeterWidget>
       <Autocomplete
         disablePortal
         sx={{ width: "200px" }}
         id="combo-box"
-        options={["elec", "water", "all meters"]}
+        options={["elec", "water", "all"]}
         renderInput={(params) => (
           <TextField {...params} label="Select utility" />
         )}
