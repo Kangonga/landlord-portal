86c7fad0b490c5b95d856912ddd3c4d27dcad623
Kangonga <guchu413@gmail.com>
2023-07-06 10:00:08 +0300

    feat: change displayed meters and sub meters based on the utility

--- src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
+++ src/features/mainMeterFeatures/components/widgets/MeterWidgets.tsx
@@ -1,9 +1,31 @@
-import { useAppSelector } from "@/globalHooks";
+import { useAppDispatch, useAppSelector } from "@/globalHooks";
 import { MeterWidget, WidgetsContainer } from "./styles";
 import { Autocomplete, TextField } from "@mui/material";
+import { utilityActions } from "@/features/utilitySlice";
+import { SyntheticEvent, useState } from "react";
 
 export default function MeterWidgets() {
   const utilityState = useAppSelector((state) => state.utility.data);
+  const dispatch = useAppDispatch();
+  const [util, setUtil] = useState("all");
+  console.log(util);
+  const getMotherMeterIds = (utility: string) => {
+    if (utility === "all") {
+      return utilityState.mm.map((mm) => mm.accNo);
+    }
+    return utilityState.mm
+      .filter((mm) => mm.utilityType === utility)
+      .map((mm) => mm.accNo);
+  };
+  const getSubMeters = () => {
+    if (util === "all") {
+      return utilityState.mm.map((mm) => mm.sm.map((sm) => sm.meterNo));
+    }
+    return utilityState.mm
+      .filter((mm) => mm.utilityType === util)
+      .map((mm) => mm.sm.map((sm) => sm.meterNo));
+  };
+
   return (
     <WidgetsContainer>
       <MeterWidget>
@@ -22,13 +44,15 @@ export default function MeterWidgets() {
         renderInput={(params) => (
           <TextField {...params} label="Select utility" />
         )}
+        value={util}
         noOptionsText="No meter found"
+        onChange={(event: SyntheticEvent, value) => setUtil(value as string)}
       />
       <Autocomplete
         disablePortal
         sx={{ width: "200px" }}
         id="combo-box"
-        options={["123134421", "873243432", "all meters"]}
+        options={getMotherMeterIds(util)}
         renderInput={(params) => <TextField {...params} label="Select meter" />}
         noOptionsText="No meter found"
       />
@@ -36,7 +60,7 @@ export default function MeterWidgets() {
         disablePortal
         sx={{ width: "200px" }}
         id="combo-box"
-        options={["123134421", "873243432", "all m-paya meters"]}
+        options={[...String(getSubMeters()).split(","), "all"]}
         renderInput={(params) => (
           <TextField {...params} label="Select m-paya meter" />
         )}
