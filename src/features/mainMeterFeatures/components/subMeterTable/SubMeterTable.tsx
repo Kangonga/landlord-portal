ddf6fbf8351dd1559f8421424bfc9c9e0d8ecca4
Kangonga <guchu413@gmail.com>
2023-07-07 22:20:09 +0300

    feat: update the sub meter table

--- src/features/mainMeterFeatures/components/subMeterTable/SubMeterTable.tsx
+++ src/features/mainMeterFeatures/components/subMeterTable/SubMeterTable.tsx
@@ -1,21 +1,28 @@
-import { Box, Typography } from '@mui/material'
-import { DataGrid, GridToolbar } from '@mui/x-data-grid'
-import { subMeterTableColumns, subMeterTableRows } from './subMeterTableDataStructure'
+import { Box, Typography } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { subMeterTableColumns } from "./subMeterTableDataStructure";
 
-export default function SubMeterTable() {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function SubMeterTable(props: any) {
   return (
     <Box>
-        <Typography textAlign='center' fontSize='1.2rem' color='gray'>M-paya meters Summary</Typography>
-        <DataGrid 
-            rows={subMeterTableRows}
-            columns={subMeterTableColumns}
-            sx={{
-              border:'2px solid #cbd5e1'
-            }}
-            slots={{
-              toolbar:GridToolbar
-            }}
-        />
+      <Typography textAlign="center" fontSize="1.2rem" color="gray">
+        M-paya meters Summary
+      </Typography>
+      <DataGrid
+        rows={props.summaryRow}
+        columns={subMeterTableColumns}
+        sx={{
+          border: "2px solid #cbd5e1",
+        }}
+        slots={{
+          toolbar: GridToolbar,
+        }}
+        initialState={{
+          pagination: { paginationModel: { pageSize: 5 } },
+        }}
+        pageSizeOptions={[5, 10, 20]}
+      />
     </Box>
-  )
+  );
 }
