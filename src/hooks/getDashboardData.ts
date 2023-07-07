19566a05e2d962c68de8536907a3e749dc3ca912
Kangonga <guchu413@gmail.com>
2023-07-07 22:39:08 +0300

    refactor: use remote api data

--- src/hooks/getDashboardData.ts
+++ src/hooks/getDashboardData.ts
@@ -1,22 +1,15 @@
 import axios from "axios";
 
-// export const getDashboardData = async (token: string, user: string) => {
-//   // const url = "https://ss.m-paya.net/api.php?src=dashboard";
-//   const url = "http://localhost:3000/data";
-//   const params = {
-//     api: {
-//       request: "viewAccount",
-//       user,
-//       token,
-//     },
-//   };
-//   // const response = await axios.post(url, params);
-//   // return response.data;
-//   const jsonData = await axios.get(url);
-//   return jsonData.data;
-// };
-export const getDashboardData = async () => {
-  const url = "http://localhost:3000/data";
-  const jsonData = await axios.get(url);
+export const getDashboardData = async (token: string, user: string) => {
+  const url = "https://ss.m-paya.net/api.php?src=dashboard";
+  const params = {
+    api: {
+      request: "viewAccount",
+      user,
+      token,
+    },
+  };
+
+  const jsonData = await axios.post(url, params);
   return jsonData.data;
 };
