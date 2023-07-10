febc32559c594504f50856d5923d5a1224a7fa23
Kangonga <guchu413@gmail.com>
2023-07-10 07:26:25 +0300

    feat: clear redux store on log out

--- src/shared/sidebar/Sidebar.tsx
+++ src/shared/sidebar/Sidebar.tsx
@@ -1,22 +1,32 @@
-import * as React from 'react';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { Drawer, DrawerHeader, SideBarItem } from './styles';
-import { images } from '@/assets';
-import { Box, SvgIconProps } from '@mui/material';
-import { ElectricMeterOutlined, Home, LiveHelpOutlined, LogoutOutlined, Person3Outlined } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { Drawer, DrawerHeader, SideBarItem } from "./styles";
+import { images } from "@/assets";
+import { Box, SvgIconProps } from "@mui/material";
+import {
+  ElectricMeterOutlined,
+  Home,
+  LiveHelpOutlined,
+  LogoutOutlined,
+  Person3Outlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "@/globalHooks";
+import { utilityActions } from "@/features/utilitySlice";
+import { authActions } from "@/features/authentication/authSlice";
+import { persistor } from "@/store";
 
 interface sidebarprops {
   open: boolean;
-  displayText:string;
-  route:string;
+  displayText: string;
+  route: string;
   icon: SvgIconProps;
 }
 
@@ -25,81 +35,105 @@ export default function Sidebar() {
   const handleDrawerOpen = () => {
     setOpen(!open);
   };
-  const sidebaritems:sidebarprops[] = [
+  const sidebaritems: sidebarprops[] = [
     {
-      displayText:'Dashboard',
-      route:'',
-      icon:<Home sx={{color:'#f1f2f3'}}/>,
-      open:open
-    },
-    // {
-    //   displayText:'Buildings',
-    //   route:'buildings',
-    //   icon:<BusinessOutlined sx={{color:'#f1f2f3'}}/>,
-    //   open:open
-    // },
-    {
-      displayText:'Meters',
-      route:'mainMeters',
-      icon:<ElectricMeterOutlined sx={{color:'#f1f2f3'}}/>,
-      open:open
+      displayText: "Dashboard",
+      route: "",
+      icon: <Home sx={{ color: "#f1f2f3" }} />,
+      open: open,
     },
     {
-      displayText:'Help',
-      route:'help',
-      icon:<LiveHelpOutlined sx={{color:'#f1f2f3'}}/>,
-      open:open
+      displayText: "Meters",
+      route: "mainMeters",
+      icon: <ElectricMeterOutlined sx={{ color: "#f1f2f3" }} />,
+      open: open,
     },
     {
-      displayText:'Profile',
-      route:'profile',
-      icon:<Person3Outlined sx={{color:'#f1f2f3'}}/>,
-      open:open
+      displayText: "Help",
+      route: "help",
+      icon: <LiveHelpOutlined sx={{ color: "#f1f2f3" }} />,
+      open: open,
     },
-  ] 
+    {
+      displayText: "Profile",
+      route: "profile",
+      icon: <Person3Outlined sx={{ color: "#f1f2f3" }} />,
+      open: open,
+    },
+  ];
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <Drawer variant="permanent" open={open}  PaperProps={{sx:{backgroundColor: "#5A57FF", color:'white'}}}>
+      <Drawer
+        variant="permanent"
+        open={open}
+        PaperProps={{ sx: { backgroundColor: "#5A57FF", color: "white" } }}
+      >
         <DrawerHeader open={open}>
-          {open && <img src={images.logo} alt='M-paya logo' />}
+          {open && <img src={images.logo} alt="M-paya logo" />}
           <IconButton onClick={handleDrawerOpen}>
-            <MenuIcon style={{color:'#f1f2f3'}} />
+            <MenuIcon style={{ color: "#f1f2f3" }} />
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <List sx={{display:'flex', flexDirection:'column',justifyContent:'space-between', gap:'2rem'}}>
-           <div>
-             {sidebaritems.map((sidebaritem, index)=>{
-              return (
-                <SideBarIcon {...sidebaritem} key={index}/>              )
-           })}
+        <List
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            gap: "2rem",
+          }}
+        >
+          <div>
+            {sidebaritems.map((sidebaritem, index) => {
+              return <SideBarIcon {...sidebaritem} key={index} />;
+            })}
           </div>
-          <SideBarIcon 
+          <SideBarIcon
             {...{
-              displayText:'Log Out',
-              route:'/',
-              icon:<LogoutOutlined sx={{color:'#f1f2f3'}}/>,
-              open:open
+              displayText: "Log Out",
+              route: "/",
+              icon: <LogoutOutlined sx={{ color: "#f1f2f3" }} />,
+              open: open,
             }}
           />
         </List>
       </Drawer>
-      </Box>
+    </Box>
   );
 }
 
-
-const SideBarIcon = (props:sidebarprops)=>{
-  const navigate = useNavigate()
-  return(
-    <SideBarItem open={props.open} onClick={():void=>navigate(props.route)}>
-      <ListItemButton sx={{mt:props.route==='/logout'?'7rem':''}}>
+const clearStore = async () => {
+  try {
+    await persistor.purge(); // Clears the entire Redux Persist store
+    console.log("Redux Persist data cleared successfully");
+  } catch (error) {
+    console.error("Failed to clear Redux Persist data:", error);
+  }
+};
+const SideBarIcon = (props: sidebarprops) => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    dispatch(utilityActions.logOut);
+    dispatch(authActions.logout);
+    clearStore();
+    location.reload();
+  };
+  return (
+    <SideBarItem
+      open={props.open}
+      onClick={(): void => {
+        navigate(props.route);
+        props.displayText === "Log Out" ? handleLogOut() : "";
+      }}
+    >
+      <ListItemButton sx={{ mt: props.route === "/logout" ? "7rem" : "" }}>
         <ListItemIcon>
           <>{props.icon}</>
         </ListItemIcon>
         <ListItemText primary={props.displayText} />
       </ListItemButton>
     </SideBarItem>
-  )
-}
+  );
+};
