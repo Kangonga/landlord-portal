5dec75f8f03adfee3e096bd86534fdc2f023fb3b
Kangonga <guchu413@gmail.com>
2023-07-10 07:48:25 +0300

    style: add a nested container to contain the data tables

--- src/features/mainMeterFeatures/components/mainMeterDataTable/MeterDataTables.tsx
+++ src/features/mainMeterFeatures/components/mainMeterDataTable/MeterDataTables.tsx
@@ -22,57 +22,76 @@ export default function MeterDataTables() {
   const subMeterSummary = useGetSubMeterSummary();
   return (
     <MeterContainer>
-      {rows.length > 0 && (
-        <Box>
-          <Typography
-            sx={{ textAlign: "center", color: "gray", fontSize: "1.2rem" }}
-          >
-            Payments In
-          </Typography>
-          <DataGrid
-            rows={rows}
-            columns={payInColumns}
-            slots={{
-              toolbar: GridToolbar,
-            }}
-            sx={{
-              border: "2px solid #cbd5e1",
-            }}
-            initialState={{
-              pagination: { paginationModel: { pageSize: 5 } },
-            }}
-            pageSizeOptions={[5, 10, 50]}
-            autoHeight
-          />
-        </Box>
+      {(rows.length > 0 ||
+        payOutRows.length > 0 ||
+        subMeterSummary.length > 0) && (
+        <Typography fontSize="1.3rem" color="blue" textAlign="center">
+          Meter Details
+        </Typography>
       )}
-      {payOutRows.length > 0 && (
-        <Box>
-          <Typography
-            sx={{ textAlign: "center", color: "gray", fontSize: "1.2rem" }}
-          >
-            Payments Out
-          </Typography>
-          <DataGrid
-            rows={payOutRows}
-            columns={payOutColumns}
-            autoHeight
-            sx={{
-              border: "2px solid #cbd5e1",
-            }}
-            slots={{
-              toolbar: GridToolbar,
-            }}
-            initialState={{
-              pagination: { paginationModel: { pageSize: 5 } },
-            }}
-            pageSizeOptions={[5, 10, 20]}
-          />
-        </Box>
-      )}
-      {subMeterSummary.length > 0 && (
-        <SubMeterTable summaryRow={subMeterSummary} />
-      )}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          gap: "1rem",
+          padding: ".75rem",
+        }}
+      >
+        {rows.length > 0 && (
+          <Box>
+            <Typography
+              sx={{ textAlign: "center", color: "gray", fontSize: "1.2rem" }}
+            >
+              Payments In
+            </Typography>
+            <DataGrid
+              rows={rows}
+              columns={payInColumns}
+              slots={{
+                toolbar: GridToolbar,
+              }}
+              sx={{
+                border: "2px solid #cbd5e1",
+                width: "max-content",
+              }}
+              initialState={{
+                pagination: { paginationModel: { pageSize: 5 } },
+              }}
+              pageSizeOptions={[5, 10, 50]}
+              autoHeight
+            />
+          </Box>
+        )}
+        {payOutRows.length > 0 && (
+          <Box>
+            <Typography
+              sx={{ textAlign: "center", color: "gray", fontSize: "1.2rem" }}
+            >
+              Payments Out
+            </Typography>
+            <DataGrid
+              rows={payOutRows}
+              columns={payOutColumns}
+              autoHeight
+              sx={{
+                border: "2px solid #cbd5e1",
+                width: "max-content",
+              }}
+              slots={{
+                toolbar: GridToolbar,
+              }}
+              initialState={{
+                pagination: { paginationModel: { pageSize: 5 } },
+              }}
+              pageSizeOptions={[5, 10, 20]}
+            />
+          </Box>
+        )}
+        {subMeterSummary.length > 0 && (
+          <SubMeterTable summaryRow={subMeterSummary} />
+        )}
+      </Box>
     </MeterContainer>
   );
 }
