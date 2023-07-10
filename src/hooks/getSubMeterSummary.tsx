f28ae31642f0525ce773597a97c8bfa441b3b4a0
Kangonga <guchu413@gmail.com>
2023-07-10 11:25:54 +0300

    feat: create summary interface and missing properties on initialisation

--- src/hooks/getSubMeterSummary.tsx
+++ src/hooks/getSubMeterSummary.tsx
@@ -1,11 +1,19 @@
 import { useAppSelector } from "@/globalHooks";
-
+interface summaryInterface {
+  id: string;
+  regDate: string;
+  lastPayment: string;
+}
 export default function useGetSubMeterSummary() {
   const activeMeter = useAppSelector((state) => state.meter);
   const summaryArr = [];
   if (activeMeter.sm) {
     for (const sm of activeMeter.sm) {
-      const summary = {};
+      const summary: summaryInterface = {
+        id: "",
+        regDate: "",
+        lastPayment: "",
+      };
       summary.id = sm.meterNo;
       summary.regDate = sm.regDate ? sm.regDate.split(" ")[0] : "not set";
       summary.lastPayment = sm.payments
