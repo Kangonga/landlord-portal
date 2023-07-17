4e796628fbb980f0a82d60b378a6f5064ae2d12e
Kangonga <guchu413@gmail.com>
2023-07-17 08:05:56 +0300

    refactor: format code

--- src/pages/profile/styles.ts
+++ src/pages/profile/styles.ts
@@ -1,61 +1,64 @@
 import { styled } from "@mui/material";
 
-export const ProfilePageContainer = styled('div')(({  })=> ({
-    display:'flex',
-    flexDirection:'column',
-    height:'100dvh'
-}))
+export const ProfilePageContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  minHeight: "100dvh",
+}));
 
-export const ProfileTopSection = styled('div')(({ theme })=> ({
-    display:'flex',
-    backgroundColor:'#f3f4f6',
-    flexDirection:'row',
-    justifyContent:'flex-start',
-    alignItems:'center',
-    padding:'1rem 3rem',
-    '.nameContainer':{
-        display:'flex',
-        flex:'2',
-        alignItems:'center',
-        flexDirection:'column',
-        gap:'.5rem',
-        '.name':{
-            fontSize:'1.3rem',
-            fontWeight:'bold',
-            textAlign:'start',
-            width:'150px'
-        },
-        '.email':{
-            fontSize:'1rem',
-            color:'#374151',
-            textAlign:'start',
-            width:'150px'
-        }
+export const ProfileTopSection = styled("div")(({ theme }) => ({
+  display: "flex",
+  backgroundColor: "#f3f4f6",
+  flexDirection: "row",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  padding: "1rem 3rem",
+  ".nameContainer": {
+    display: "flex",
+    flex: "2",
+    alignItems: "center",
+    flexDirection: "column",
+    gap: ".5rem",
+    ".name": {
+      fontSize: "1.3rem",
+      fontWeight: "bold",
+      textAlign: "start",
+      width: "max-content",
     },
-    [( theme.breakpoints.down('sm') )]:{
-        flexDirection:'column'
-    }
-}))
+    ".email": {
+      fontSize: "1rem",
+      color: "#374151",
+      textAlign: "start",
+      width: "max-content",
+    },
+  },
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+}));
 
-export const Form = styled('form')(({  })=>({
-    width:'65%',
-    padding:'1rem 0 1rem 5rem',
-    display:'flex',
-    flexDirection:'column',
-    gap:'1rem',
-    '& > div':{
-        display:'flex',
-        justifyContent:'space-between',
-        alignItems:'center'
-    },
-    'input':{
-        borderRadius:'10px',
-        border:'none',
-        outline:'thin',
-        backgroundColor:'lightgray',
-        height:'3rem',
-        width:'300px',
-        padding:'.5rem',
-        fontSize:'1rem'
-    }
-}))
+export const Form = styled("form")(() => ({
+  width: "65%",
+  padding: "1rem 0 1rem 5rem",
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  "& > div": {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  label: {
+    minWidth: "100px",
+  },
+  input: {
+    borderRadius: "10px",
+    border: "none",
+    outline: "thin",
+    backgroundColor: "lightgray",
+    height: "3rem",
+    width: "300px",
+    padding: ".5rem",
+    fontSize: "1rem",
+  },
+}));
