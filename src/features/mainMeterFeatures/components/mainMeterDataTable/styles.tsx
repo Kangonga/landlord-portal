f385e1abc515fb316c7605a72c3f2998757bc9ff
Kangonga <guchu413@gmail.com>
2023-07-17 08:07:35 +0300

    style: change height to minheight

--- src/features/mainMeterFeatures/components/mainMeterDataTable/styles.tsx
+++ src/features/mainMeterFeatures/components/mainMeterDataTable/styles.tsx
@@ -1,9 +1,10 @@
 import { styled } from "@mui/material";
 
-export const MeterContainer = styled("div")(({}) => ({
+export const MeterContainer = styled("div")(() => ({
   display: "flex",
   flexDirection: "column",
   flexWrap: "wrap",
   gap: "1rem",
   padding: ".75rem",
+  minHeight: "100dvh",
 }));
