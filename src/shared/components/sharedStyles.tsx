7d057dc2fa6d0a876ef519dae0b35d313e5b6b81
Kangonga <guchu413@gmail.com>
2023-07-17 08:05:39 +0300

    refactor: format code

--- src/shared/components/sharedStyles.tsx
+++ src/shared/components/sharedStyles.tsx
@@ -1,12 +1,12 @@
 import { styled } from "@mui/material";
 
-export const Container = styled('div')(({  })=>({
-    display:'flex',
-    color:'black',
-}))
-export const ComponentContainer = styled('div')(({  })=>({
-    display:'flex',
-    flexDirection:'column',
-    flex:'2',
-    backgroundColor:'#e0e7ff'
-}))
+export const Container = styled("div")(() => ({
+  display: "flex",
+  color: "black",
+}));
+export const ComponentContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  flex: "2",
+  backgroundColor: "#e0e7ff",
+}));
