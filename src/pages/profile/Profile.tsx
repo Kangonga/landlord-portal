80963600ae0f08634e2685fa8a62858b41645dcf
Kangonga <guchu413@gmail.com>
2023-07-17 08:06:46 +0300

    feat: update profile page to render accurate data

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,36 +1,43 @@
 import { Avatar, Divider, Typography } from "@mui/material";
 import { Form, ProfilePageContainer, ProfileTopSection } from "./styles";
-import { images } from "@/assets";
+import { useAppSelector } from "@/globalHooks";
 
 export default function Profile() {
+  const utilityState = useAppSelector((state) => state.utility);
   return (
     <ProfilePageContainer>
       <ProfileTopSection>
         <section className="avatar">
-          <Avatar sx={{width:'150px', height:'150px'}} src={images.profile}/>
+          <Avatar sx={{ width: "150px", height: "150px" }} />
         </section>
         <section className="nameContainer">
-          <span className="name">John Doe</span>
-          <span className="email">jdoe@mail.com</span>
+          <span className="name">{utilityState.data.Names}</span>
+          <span className="email">{utilityState.data.email}</span>
+          <span className="email">
+            {utilityState.data.phone1 ? utilityState.data.phone1 : ""}
+          </span>
         </section>
       </ProfileTopSection>
       <Form>
-        <Typography fontSize='2rem'>My personal details</Typography>
+        <Typography fontSize="2rem">My personal details</Typography>
         <Divider />
         <div>
           <label htmlFor="Full name">Full name</label>
-          <input type="text" disabled value={'John Doe'}/>
+          <input type="text" disabled value={utilityState.data.Names} />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" disabled value={'jdoe@mail.com'}/>
+          <input
+            type="text"
+            disabled
+            value={utilityState.data.email ? utilityState.data.email : ""}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" disabled value={'*******'}/>
+          <input type="password" disabled value={"*******"} />
         </div>
       </Form>
-
     </ProfilePageContainer>
-  )
+  );
 }
