bff569908f16f20074e748620887ea4319918a1b
Kangonga <guchu413@gmail.com>
2023-07-17 08:07:10 +0300

    refactor: format code

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,8 +1,9 @@
 import { styled } from "@mui/material";
 
-export const HomeContainer = styled('div')(({})=>({
-    display:'flex',
-    flexDirection:'column',
-    gap:'1rem',
-    padding:'.75rem'
-}))
+export const HomeContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  padding: ".75rem",
+  minHeight: "100dvh",
+}));
