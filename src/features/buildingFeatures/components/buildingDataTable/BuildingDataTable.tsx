7655174715071180bd194d309aef5d453d0e9b67
Kangonga <guchu413@gmail.com>
2023-08-12 19:04:26 +0300

    feat: update the current month functionality

--- src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
+++ src/features/buildingFeatures/components/buildingDataTable/BuildingDataTable.tsx
@@ -2,18 +2,20 @@ import { Box, Typography } from "@mui/material";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { useColumns } from "./buildingTableDataStructure";
 import { summaryInterface } from "@/hooks/getUtilitySummary";
+import { getCurrentMonthName } from "@/utils/getStatementPeriods";
 
 export default function BuildingDataTable({
   rows,
 }: {
   rows: summaryInterface[];
 }) {
+  const month = getCurrentMonthName();
   return (
     <Box>
       <Typography
         sx={{ fontSize: "1.2rem", textAlign: "center", color: "gray" }}
       >
-        July 2023 Building summary
+        {month} 2023 Building summary
       </Typography>
       <DataGrid
         columns={useColumns()}
