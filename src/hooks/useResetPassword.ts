ae4fce6a3bf5d4e1ae4d5468e7f6e60212440fe3
Kangonga <guchu413@gmail.com>
2023-08-14 03:36:54 +0300

    feat: update endpoint urls

--- src/hooks/useResetPassword.ts
+++ src/hooks/useResetPassword.ts
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
-export default async function useResetPassword(phone:string){
-    const url = 'https://enerlytics.m-paya.net/user?'
-    const params = {
-        api:{
-            request:'resetPassword'
-        },
-        input:{
-            phone
-        }
-    }
-    const response = await axios.post(url,params)
-    return response.data
+export default async function useResetPassword(phone: string) {
+  const url = "https://ss.m-paya.net/api.php?src=user";
+  const params = {
+    api: {
+      request: "resetPassword",
+    },
+    input: {
+      phone,
+    },
+  };
+  const response = await axios.post(url, params);
+  return response.data;
 }
--- src/hooks/useVerifyOtp.ts
+++ src/hooks/useVerifyOtp.ts
@@ -1,16 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 
-export default async function useVerifyOtp(otp:string, userId:string){
-    const url = 'https://enerlytics.m-paya.net/user?'
-    const params = {
-        api:{
-            request:'otp',
-            user:userId
-        },
-        input:{
-            otp
-        }
-    }
-    const response = await axios.post(url,params)
-    return response.data
+export default async function useVerifyOtp(otp: string, userId: string) {
+  const url = "https://ss.m-paya.net/api.php?src=user";
+  const params = {
+    api: {
+      request: "otp",
+      user: userId,
+    },
+    input: {
+      otp,
+    },
+  };
+  const response = await axios.post(url, params);
+  return response.data;
 }
