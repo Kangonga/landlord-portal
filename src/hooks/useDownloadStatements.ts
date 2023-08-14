fe2600243961d94cb8df2a31b9eea9e0f1daa6c3
Kangonga <guchu413@gmail.com>
2023-08-14 03:12:28 +0300

    feat: create utility function to download the statements

---
+++ src/hooks/useDownloadStatements.ts
@@ -0,0 +1,61 @@
+import axios, { AxiosRequestConfig } from "axios";
+import fs from "fs";
+
+interface statementProps {
+  account: any;
+  userId: any;
+  token: any;
+  from: string;
+  to: string;
+}
+export default async function getDownloadStatements({
+  account,
+  userId,
+  token,
+  from,
+  to,
+}: statementProps) {
+  const url = "https://ss.m-paya.net/api.php?src=dashboard";
+  const params = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      api: {
+        request: "viewStatement",
+        user: userId,
+        token: token,
+      },
+      input: {
+        from: from,
+        to: to,
+        account: account,
+      },
+    }),
+  };
+  const params2 = {
+    api: {
+      request: "viewStatement",
+      user: userId,
+      token: token,
+    },
+    input: {
+      from: from,
+      to: to,
+      account: account,
+    },
+  };
+  const result = await fetch(url, params);
+  const blob = await result.blob();
+  const newurl = window.URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = newurl;
+  link.setAttribute("download", `${Date.now()}.xlsx`);
+  document.body.appendChild(link);
+  link.click();
+
+  // Clean up the blob URL after the download
+  window.URL.revokeObjectURL(newurl);
+  console.log("params sent", params);
+}
